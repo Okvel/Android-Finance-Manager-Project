d96fdb2081233874b30673667709de8bdc36818b
Sergey Levko <okvel.s@gmail.com>
2015-10-09 01:18:33 +0300

    Create mainActivity class and layout

---
+++ FinanceManager/app/src/main/java/net/lion/okvel/financemanager/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package net.lion.okvel.financemanager.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.lion.okvel.financemanager.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
