927df780a9056d06f189b5e59eafcfdd4b79f964
Sergey Levko <okvel.s@gmail.com>
2015-10-11 19:14:37 +0300

    [CP-3] Added Toolbar and menu.

--- FinanceManager/app/src/main/java/net/lion/okvel/financemanager/activity/MainActivity.java
+++ FinanceManager/app/src/main/java/net/lion/okvel/financemanager/activity/MainActivity.java
@@ -1,17 +1,28 @@
 package net.lion.okvel.financemanager.activity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import net.lion.okvel.financemanager.R;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends Activity
 {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
