cba09b9777ac9e46807820a0ca36b0270a3d0eaa
Sergey Levko <okvel.s@gmail.com>
2015-10-29 01:25:57 +0300

    [CP-4] Start to implementing NavigationView.

--- FinanceManager/app/src/main/java/net/lion/okvel/financemanager/activity/MainActivity.java
+++ FinanceManager/app/src/main/java/net/lion/okvel/financemanager/activity/MainActivity.java
@@ -1,19 +1,19 @@
 package net.lion.okvel.financemanager.activity;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import net.lion.okvel.financemanager.R;
 
-public class MainActivity extends Activity
+public class MainActivity extends AppCompatActivity
 {
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
