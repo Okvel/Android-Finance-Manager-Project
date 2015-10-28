66041bb6bfb094b39d346d7d1b6a9971d49cce90
Sergey Levko <okvel.s@gmail.com>
2015-10-29 02:03:51 +0300

    [CP-4] Added basic NavigationView.

--- FinanceManager/app/src/main/java/net/lion/okvel/financemanager/activity/MainActivity.java
+++ FinanceManager/app/src/main/java/net/lion/okvel/financemanager/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package net.lion.okvel.financemanager.activity;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,21 +9,30 @@ import net.lion.okvel.financemanager.R;
 
 public class MainActivity extends AppCompatActivity
 {
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolBar();
+        initNavigationView();
     }
 
     private void initToolBar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView()
+    {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
